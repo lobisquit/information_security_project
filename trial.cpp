8f3f108052fa04535dedea0e9b11c63629672c56
Enrico Lovisotto <enricolovisotto@gmail.com>
2018-01-25 22:10:11 +0100

    Test with gmp RNG

--- trial.cpp
+++ trial.cpp
@@ -2,19 +2,11 @@
 
 #include <gmp.h>
 #include <gmpxx.h> // needed for C++ adapter
-#include <pbc.h>
+// #include <pbc.h>
 
 int main(int argc, char** argv) {
-	// cannot construct with string directly, lol
-	// only assignment works
-	mpz_class integ, other;
-	integ = 100;
-	other = "10";
-
-	std::cout << (integ + other) << "\n";
-
-	// need to pre-multiply and assign, otherwise does
-	// not work (laziness probably for performance)
-	integ = integ * 2;
-	gmp_printf("Integer %Zd\n", integ.get_mpz_t());
+	gmp_randclass rng(gmp_randinit_default);
+	rng.seed(time(NULL));
+	mpf_class number = rng.get_f();
+	std::cout << number << "\n";
 }
